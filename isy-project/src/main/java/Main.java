bc30214dea883435ea88758e45c0e57552ccdaf4
norm095 <norman.aga095@gmail.com>
2025-09-09 15:15:58 +0200

    commit_test2

--- isy-project/src/main/java/Main.java
+++ isy-project/src/main/java/Main.java
@@ -14,6 +14,7 @@ public class Main {
             System.out.println("i = " + i);
 
             System.out.println("Hello Group 5");
+            System.out.println("Test nr 2");
         }
     }
 }
