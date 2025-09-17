e91c04ff304e74488009ece7df711574ad978780
Aimee481795 <ajb.severijn@st.hanze.nl>
2025-09-17 14:01:28 +0200

    speelbord

--- tic-tak-toe/src/Main.java
+++ tic-tak-toe/src/Main.java
@@ -2,10 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
+    //Dit is het startpunt van het programma.
+        // bepaald wat waneer word geladen
 
     }
 }
---
+++ tic-tak-toe/src/TicTacToe.java
@@ -0,0 +1,5 @@
+public class TicTacToe {
+}
+// spelregels (logica)
+// dus bord bijhouden, speler wisselen, zetten
+// en win condiete
